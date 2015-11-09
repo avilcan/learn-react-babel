55739e064448b42fe1186f3110a039242f8fb084
alexandravilcan <alexandravilcan@yahoo.com>
2015-11-09 19:21:12 +0200

    add initial source files

---
+++ PageContent.jsx
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+ReactDOM.render(
+    <h1>Welcome to "Learn to think in React and Flux"</h1>,
+    document.getElementById("content"));
